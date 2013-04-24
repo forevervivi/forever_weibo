dd61e1c3229548b4d2eb37ce5e1ff52ddf3d970a
forevervivi <526312034@qq.com>
2013-04-24 19:17:23 +0800

    调整OauthActivity中spinner下拉框位置

--- src/com/forever/weibo/LoginActivity.java
+++ src/com/forever/weibo/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.forever.weibo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,9 +10,11 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.BaseAdapter;
@@ -37,6 +40,7 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
+
 		bt_submit = (Button) findViewById(R.id.login_submit);
 		bt_authorize = (Button) findViewById(R.id.login_authorize);
 		spinner = (Spinner) findViewById(R.id.login_spinner);
@@ -59,16 +63,17 @@ public class LoginActivity extends Activity {
 				userMap.put("head", u.getUser_head());
 				userList.add(userMap);
 			}
-
 			spinner.setAdapter(new _Adapter(userList));
+			spinner.setDropDownVerticalOffset(0);//调整下拉框位置
+			
 
 			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 				@Override
 				public void onItemSelected(AdapterView<?> parent, View view,
 						int position, long id) {
-					 current = userData.get(position);
-					
+					current = userData.get(position);
+
 				}
 
 				@Override
@@ -77,6 +82,7 @@ public class LoginActivity extends Activity {
 			});
 		}
 	}
+	
 
 	/**
 	 * 自定义Adapter，用于spinner显示图片和文字
@@ -144,19 +150,19 @@ public class LoginActivity extends Activity {
 			case R.id.login_submit:
 				// 保存当前登录的用户
 				UserCurrent.currentUser = current;
-				
+
 				Intent intent1 = new Intent(LoginActivity.this,
 						HomeActivity.class);
 				startActivity(intent1);
 				LoginActivity.this.finish();
-				
+
 				Log.i("user", UserCurrent.currentUser.getUser_name());
 				break;
 			case R.id.login_authorize:
 				Intent intent2 = new Intent(LoginActivity.this,
 						OAuthActivity.class);
 				startActivity(intent2);
-				//LoginActivity.this.finish();
+				// LoginActivity.this.finish();
 				break;
 			default:
 				break;
